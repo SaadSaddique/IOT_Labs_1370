88e8db27c3ba130dedc3cb205e0f530863091c9c
SaadSaddique <saad.saddique1370@gmail.com>
2025-06-14 17:20:02 +0500

    done

---
+++ Lab6/boot.py
@@ -0,0 +1,28 @@
+# setup static ip for esp32
+
+import network
+import utime as time
+
+WIFI_SSID = 'Huzaifa'
+WIFI_PASS = '12345678'
+
+# Static IP configuration
+STATIC_IP = "192.168.78.60"  # Replace with your desired static IP/
+SUBNET_MASK = "255.255.255.0"
+GATEWAY = "192.168.78.213"  # Replace with your router's IP/hotspot
+DNS_SERVER = "8.8.8.8"  # Google DNS
+
+
+
+print("Connecting to WiFi network '{}'".format(WIFI_SSID))
+wifi = network.WLAN(network.STA_IF)
+wifi.active(False)
+wifi.active(True)
+
+wifi.ifconfig((STATIC_IP, SUBNET_MASK, GATEWAY, DNS_SERVER))
+
+wifi.connect(WIFI_SSID, WIFI_PASS)
+while not wifi.isconnected():
+    time.sleep(1)
+    print('WiFi connect retry ...')
+print('WiFi IP:', wifi.ifconfig()[0])
